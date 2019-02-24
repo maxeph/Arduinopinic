fc2bc64f97ab0ddc469505e2d3817d737c3daf2e
maxeph <s.manguette@gmail.com>
2019-02-24 15:04:39 +0000

    Correcting previous commit

--- install.sh
+++ install.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
+
+echo -e "\n\033[32m######## installing virtualenv ########\033[0m"
 sudo apt-get install virtualenv -y
-git clone https://github.com/maxeph/frontend_Arduinopinic.git
-git clone https://github.com/maxeph/frontend_Arduinopinic.git frontend_Arduinopinic/daemon
-chmod 777 frontend_Arduinopinic/manage.py
-virtualenv venvArduinopinic && source venvArduinopinic/bin/activate
+
+echo -e "\n\033[32m######## cloning repositories ########\033[0m"
+git clone https://github.com/maxeph/frontend_Arduinopinic.git ../Arduinopinic
+git clone https://github.com/maxeph/daemon_Arduinopinic.git ../Arduinopinic/Arduinopinic/daemon
+
+echo -e "\n\033[32m######## creating virtualenv ########\033[0m"
+virtualenv ../Arduinopinic/.venv/venvArduinopinic && source ../Arduinopinic/.venv/venvArduinopinic/bin/activate
+
+echo -e "\n\033[32m######## installing dependencies ########\033[0m"
 pip install django crcmod arrow smbus2 terminaltables docopt
-python frontend_Arduinopinic/manage.py migrate
-python frontend_Arduinopinic/manage.py makemigrations
-chmod 777 frontend_Arduinopinic/daemon/piserver.py
+
+echo -e "\n\033[32m######## changing rights on executables ########\033[0m"
+chmod 777 ../Arduinopinic/manage.py
+chmod 777 ../Arduinopinic/Arduinopinic/daemon/daemon.py
+echo "Rights granted to Arduinopinic executables..."
+
+echo -e "\n\033[32m######## creating database ########\033[0m"
+python ../Arduinopinic/manage.py makemigrations
+python ../Arduinopinic/manage.py migrate
+python module/database.py
+
+mkdir ../Arduinopinic/Arduinopinic/daemon/log
