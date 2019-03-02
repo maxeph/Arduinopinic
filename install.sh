5bd44ed571240d740d48b57a0d8c0f629703a1f5
maxeph <s.manguette@gmail.com>
2019-03-02 23:46:03 +0100

    - Adaptation of installation depending of the location of the script

--- install.sh
+++ install.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+############# Get install dir into variable
+MY_PATH="`dirname \"$0\"`"
+MY_PATH="`( cd \"$MY_PATH\" && pwd )`"
+if [ -z "$MY_PATH" ] ; then
+  # error; for some reason, the path is not accessible
+  # to the script (e.g. permissions re-evaled after suid)
+  exit 1  # fail
+fi
+
+cd $PATH
+cd ..
+INSTALL_PATH=$PWD
+DJANGO_PATH=${PWD}/Arduinopinic
+DAEMON_PATH=${PWD}/Arduinopinic/Arduinopinic/daemon
+cd install_Arduinopinic
+#########
+
+############# Updating service files
+sed -i "s/REPOCHEMIN/${DJANGO_PATH//\//\\/}/g" module/APC_daemon.service
+sed -i "s/DAEMONCHEMIN/${DAEMON_PATH//\//\\/}/g" module/APC_daemon.service
+sed -i "s/REPOCHEMIN/${DJANGO_PATH//\//\\/}/g" module/APC_django.service
+
+#########
+
 echo -e "\n\033[32m######## installing virtualenv ########\033[0m"
 sudo apt-get install virtualenv -y
 
@@ -22,5 +46,15 @@ echo -e "\n\033[32m######## creating database ########\033[0m"
 python ../Arduinopinic/manage.py makemigrations
 python ../Arduinopinic/manage.py migrate
 python module/database.py
+
+echo -e "\n\033[32m######## creating services ########\033[0m"
+sudo cp module/APC_daemon.service /etc/systemd/system/APC_daemon.service
+sudo cp module/APC_django.service /etc/systemd/system/APC_django.service
+sudo systemctl enable APC_daemon.service
+sudo systemctl enable APC_django.service
+sudo systemctl start APC_daemon.service
+sudo systemctl start APC_django.service
+
+echo "Services implemented..."
 
 mkdir ../Arduinopinic/Arduinopinic/daemon/log
